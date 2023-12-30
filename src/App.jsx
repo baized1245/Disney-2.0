e8893748fe4af2b8d9088e55cbf6e8a5d4df213e
unknown <you@example.com>
2023-12-30 15:37:48 +0600

    Complete Slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header';
+import Slider from './Components/Slider';
 
 const App = () => {
   return (
     <div>
        <Header/>
+       <Slider/>
     </div>
   );
 };
