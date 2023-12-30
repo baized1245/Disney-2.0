63d27c3353a4305bbea34d621f2197a7f9bf6408
unknown <you@example.com>
2023-12-30 14:45:34 +0600

    Complete the header section with resposive design

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/Images/logo.png";
 import {
   HiHome,
@@ -8,9 +8,11 @@ import {
   HiTv,
   HiPlus,
 } from "react-icons/hi2";
+import { HiDotsVertical } from "react-icons/hi";
 import HeaderItem from "./HeaderItem";
 
 const Header = () => {
+    const [toggle,setToggle]=useState(false);
   const menu = [
     {
       name: "HOME",
@@ -41,9 +43,26 @@ const Header = () => {
     <div className="flex items-center gap-8 justify-between p-5">
       <div className="flex gap-8 items-center">
         <img src={logo} className="w-[80px] md:w-[115px]object-cover" />
-        {menu.map((item, index) => (
+       <div className="hidden md:flex gap-8">
+       {menu.map((item, index) => (
           <HeaderItem key={index} Icon={item.icon} name={item.name} />
         ))}
+       </div>
+       <div className="flex md:hidden gap-5">
+       {menu.map((item, index) => index < 3 && (
+          <HeaderItem key={index} Icon={item.icon} name={''} />
+        ))}
+        <div className="md:hidden" onClick={()=>setToggle(!toggle)}>
+            <HeaderItem name={""} Icon={HiDotsVertical}/>
+
+            {
+               toggle ? <div className="absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4">
+                {menu.map((item, index) => index > 2 && (
+              <HeaderItem key={index} Icon={item.icon} name={item.name} />
+            ))} 
+            </div> : null }
+        </div>
+       </div>
       </div>
       <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
         className='w-[40px] rounded-full'/>
