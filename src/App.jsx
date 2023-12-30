f39d4538c0e0f757fed08220f7030e1643240796
unknown <you@example.com>
2023-12-30 18:38:15 +0600

    Add ProductionHouse Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Slider from './Components/Slider';
+import ProductionHouse from './Components/ProductionHouse';
 
 const App = () => {
   return (
     <div>
        <Header/>
        <Slider/>
+       <ProductionHouse/>
     </div>
   );
 };
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -3,7 +3,7 @@ import GlobalApi from '../Services/GlobalApi'
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
 const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
 const screenWidth=window.innerWidth;
-function Slider() {
+const Slider = () => {
     const [movieList,setMovieList]=useState([]);
     const elementRef=useRef();
     useEffect(()=>{
